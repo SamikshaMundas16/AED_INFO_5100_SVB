3e85bf28f86ec708079c3d151c0f9219051a16dc
Bhavanish Dhamnaskar <dhamnaskar.b@northeastern.edu>
2022-11-22 21:02:22 -0500

    My first commit

---
+++ Business/Configuration.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.Configuration to edit this template
+ */
+package Business;
+
+/**
+ *
+ * @author DELL
+ */
+public class Configuration {
+    
+}
