90b69eda3e31bb8929a336fd4ceb9e55cc111846
SamikshaMundas16 <mundas.s@northeastern.edu>
2022-11-28 21:07:21 -0500

    adding Enterprise java class

---
+++ src/ModelNew/Enterprise.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ModelNew;
+
+/**
+ *
+ * @author Samikshan
+ */
+public class Enterprise {
+
+    protected String name;
+    protected String phone;
+
+    public Enterprise() {
+        
+    }
+    
+    public Enterprise(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getphone() {
+        return phone;
+    }
+
+    public void setphone(String phone) {
+        this.phone = phone;
+    }
+
+    public String toString() {
+        return name;
+    }
+}
+
