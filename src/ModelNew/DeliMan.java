0da8a50a3082213419db0e2b8dd2d86cfecb28b9
SamikshaMundas16 <mundas.s@northeastern.edu>
2022-11-28 20:49:56 -0500

    updated

--- src/ModelNew/DeliMan.java
+++ src/ModelNew/DeliMan.java
@@ -4,10 +4,57 @@
  */
 package ModelNew;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Samikshan
  */
-public class DeliMan {
-    
+public class DeliMan extends Person {
+
+    private String city;
+    List<Order> list = new ArrayList<>();
+
+    public DeliMan(String name, String city, String uName, String pwd) {
+        this.name = name;
+        this.pwd = pwd;
+        this.uName = uName;
+        this.city = city;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public List<Order> getList() {
+        return list;
+    }
+
+    public void setList(List<Order> list) {
+        this.list = list;
+    }
+
+    public void addOrder(Order order) {
+        list.add(order);
+    }
+
+    public Order findOrder(String orderId) {
+        for (int i = 0; i < list.size(); i++) {
+            if (orderId.equals(list.get(i).getId())) {
+                return list.get(i);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
 }
