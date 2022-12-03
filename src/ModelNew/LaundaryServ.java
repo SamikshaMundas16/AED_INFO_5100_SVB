da52409e6c21f88084cd0187a90cd69ebac11479
SamikshaMundas16 <mundas.s@northeastern.edu>
2022-12-03 11:44:43 -0500

    adding laundary services

--- src/ModelNew/LaundaryServ.java
+++ src/ModelNew/LaundaryServ.java
@@ -4,10 +4,61 @@
  */
 package ModelNew;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Samikshan
  */
 public class LaundaryServ {
-    
+    private String name;
+    private String city;
+    private String pwd;
+    private String uName;
+
+    List<Order> list = new ArrayList<>();
+
+    public LaundaryServ(String name, String city, String uName, String pwd) {
+        this.name = name;
+        this.pwd = pwd;
+        this.uName = uName;
+    }
+
+    public String getpwd() {
+        return pwd;
+    }
+
+    public void setpwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getuName() {
+        return uName;
+    }
+
+    public void setuName(String uName) {
+        this.uName = uName;
+    }
+ 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
