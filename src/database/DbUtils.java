8d0b7e116bbdfc31b4da8ccc578b15103e676b13
SamikshaMundas16 <mundas.s@northeastern.edu>
2022-12-11 06:18:53 -0500

    updating database methods

--- src/database/DbUtils.java
+++ src/database/DbUtils.java
@@ -8,6 +8,7 @@ import com.db4o.Db4oEmbedded;
 import com.db4o.ObjectContainer;
 import com.db4o.ObjectSet;
 import com.db4o.config.EmbeddedConfiguration;
+import ModelNew.systAdmin;
 
 
 /**
@@ -32,12 +33,36 @@ public class DbUtils {
     private ObjectContainer createConnection() {
         EmbeddedConfiguration config = Db4oEmbedded.newConfiguration();
         config.common().activationDepth(Integer.MAX_VALUE);
-        
+        config.common().objectClass(systAdmin.class).updateDepth(Integer.MAX_VALUE);
+        config.common().objectClass(systAdmin.class).cascadeOnUpdate(true);
 
-        ObjectContainer db = Db4oEmbedded.openFile(config, "database/ShivSagar.db4o");
+        ObjectContainer db = Db4oEmbedded.openFile(config, "database/FeelHome.db4o");
         return db;
     }
 
-    
-    
+    public synchronized void storeSystem(systAdmin system) {
+        ObjectContainer conn = createConnection();
+        conn.store(system);
+        conn.commit();
+        conn.close();
+    }
+
+    public systAdmin retrieveSystem() {
+        try {
+            ObjectContainer conn = createConnection();
+            ObjectSet<systAdmin> systems = conn.query(systAdmin.class); // Change to the object you want to save
+            systAdmin system;
+            if (systems.isEmpty()) {
+                system = systAdmin.createsystAdmin();
+            } else {
+                system = systems.get(systems.size() - 1);
+            }
+            conn.close();
+            return system;
+        } catch (Exception ex) {
+            System.out.print(ex);
+        }
+        return null;
+    }
+
 }
