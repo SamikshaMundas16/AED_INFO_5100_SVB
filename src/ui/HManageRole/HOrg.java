deae160f8a039a2127686da2d1b9b017fd10716b
SamikshaMundas16 <114256778+SamikshaMundas16@users.noreply.github.com>
2022-12-11 23:09:19 -0500

    data validation done

--- src/ui/HManageRole/HOrg.java
+++ src/ui/HManageRole/HOrg.java
@@ -311,6 +311,11 @@ public class HOrg extends javax.swing.JPanel {
             JOptionPane.showMessageDialog(this, "Org name should be at least 2 characters long.");
             return;
         }
+        
+        if(contact.length()!=10){
+            JOptionPane.showMessageDialog(this, "Enter Valid 10 digit Phone Number");
+            return;
+        }
 
         String orgType1 = orgCombo.getSelectedItem().toString();      // org-type (Phy org)
         entDir enterpriseDirc = network.getentDir();
